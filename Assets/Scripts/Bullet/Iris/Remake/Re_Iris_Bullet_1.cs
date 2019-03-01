e017d1c55d8c2cf04f4f4506981dda44745bef3a
KimHoseong <ghtjd000129@naver.com>
2019-03-01 09:31:17 +0900

    아이리스 스킬1 초석(이 브랜치는 나중에 동방 컴 브랜치와 머지해야함)

---
+++ Assets/Scripts/Bullet/Iris/Remake/Re_Iris_Bullet_1.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Re_Iris_Bullet_1 : Bullet {
+
+    Vector3 oPosition_Temp;
+
+    int bulNum;
+
+    float rotatingAngle;
+    float rotating_Temp = 0f;
+
+    public void Init_Iris_BulletLeft(int _shooterNum, int num, Vector3 aimDVector)
+    {
+        photonView.RPC("Init_Iris_BulletLeft_RPC", PhotonTargets.All, _shooterNum, num, aimDVector);
+    }
+
+    [PunRPC]
+    protected void Init_Iris_BulletLeft_RPC(int _shooterNum, int num, Vector3 aimDVector)
+    {
+        DVector = FavoriteFunction.VectorCalc(gameObject, _shooterNum == 1 ? 2 : 1);
+        bulNum = num;
+        Invoke("DestroyToServer", 5f);
+        shooterNum = _shooterNum;
+        if (shooterNum == 1)
+        {
+            oNum = 2;
+        }
+        else if (shooterNum == 2)
+        {
+            oNum = 1;
+        }
+        Move(shooterNum);
+    }
+
+    protected override void Move(int _shooterNum)
+    {
+        damage = 10;
+        speed = 6f;
+
+
+        if (bulNum == 0)
+        {
+            rotating_Temp = (2 * Mathf.PI * 0);
+        }
+        else if (bulNum == 1)
+        {
+            rotating_Temp = (2 * Mathf.PI * (Random.Range(-7f, 7f) / 360f));
+        }
+        else if (bulNum == 2)
+        {
+            rotating_Temp = (2 * Mathf.PI * (Random.Range(-7f, 7f) / 360f));
+        }
+
+        rotatingAngle = DVector.y > 0 ? Vector3.AngleBetween(Vector3.right, DVector) : -Vector3.AngleBetween(Vector3.right, DVector);
+        rotatingAngle += rotating_Temp;
+
+        if (_shooterNum == 1)
+        {
+            transform.Rotate(0f, 0f, 0f);
+        }
+
+        else
+        {
+            transform.Rotate(0f, 180f, 0f);
+        }
+
+        Vector3 dVector_Temp = DVector;
+
+        dVector_Temp.x = Mathf.Cos(rotatingAngle);
+        dVector_Temp.y = Mathf.Sin(rotatingAngle);
+
+        dVector_Temp.Normalize();
+
+        DVector = dVector_Temp;
+
+        StartCoroutine(Accel_IrisBulletLeft());
+    }
+
+    IEnumerator Accel_IrisBulletLeft()
+    {
+        speed = 6f;
+
+        while (true)
+        {
+            if (speed >= 10f)
+                break;
+
+            speed += 3 * Time.deltaTime;
+            rgbd.velocity = DVector * speed;
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Skills/Iris/RemakeVersion/Re_Iris_Skill_1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Re_Iris_Skill_1 : Skills {
+
+    public override void Excute()
+    {
+        if (isRunning)
+            return;
+
+        StartCoroutine(Shoot_IrisSkillLeft());
+        StartCoroutine(Waiting());
+
+        Debug.Log(AudioController.instance);
+        AudioController.instance.PlayEffectSound(Character.IRIS, 0);
+    }
+
+    bool isRunning = false;
+    IEnumerator Waiting()
+    {
+        isRunning = true;
+        yield return new WaitForSeconds(1 / delay);
+        isRunning = false;
+    }
+
+    IEnumerator Shoot_IrisSkillLeft()
+    {
+        Re_Iris_Bullet_1[] bul = new Re_Iris_Bullet_1[3];
+
+        yield return new WaitForSeconds(0.1f);
+
+        for (int i = 0; i < 3; i++)
+        {
+            bul[i] = PhotonNetwork.Instantiate
+                ("Iris_BulletLeft", transform.position, Quaternion.identity, 0).
+                GetComponent<Re_Iris_Bullet_1>();
+            bul[i].Init_Iris_BulletLeft(GameManager.instance.myPnum, i, GameManager.instance.Local.aimVector);
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
